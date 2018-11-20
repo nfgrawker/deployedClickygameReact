11b748c6ba4fc5fb6c9671a8e515774bc018db33
nfgrawker <nfgrawker@gmail.com>
2018-11-20 10:36:51 -0600

    Updates

---
+++ precache-manifest.09253a02f7e9ea1b7fbc63111336bca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deployedClickygameReact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2c514f434ca29c32a560",
+    "url": "/deployedClickygameReact/static/js/runtime~main.2c514f43.js"
+  },
+  {
+    "revision": "acd12248d67b27c78ac1",
+    "url": "/deployedClickygameReact/static/js/main.acd12248.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/deployedClickygameReact/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "acd12248d67b27c78ac1",
+    "url": "/deployedClickygameReact/static/css/main.111d90eb.chunk.css"
+  },
+  {
+    "revision": "ced911838bb332702a92eb9a5b08ca26",
+    "url": "/deployedClickygameReact/index.html"
+  }
+];
